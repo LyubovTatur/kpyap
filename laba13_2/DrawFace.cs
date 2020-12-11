75acfd3a1db4aef95d333c244c0cf65cfd11e832
1184169 <lubatatur58@gmail.com>
2020-12-11 10:30:05 +0300

    13

---
+++ laba13_2/DrawFace.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba13_2
+{
+    static class DrawFace
+    {
+        static public string Happy() {
+            return 
+            "_______ \n" +
+            "| | | |\n" +
+            "|.   .|\n" +
+            "| \\_/ |\n" +
+            "_______\n";
+        }
+        static public string Sad()
+        {
+            return
+            "_______ \n" +
+            "| / \\ |\n" +
+            "|  _  |\n" +
+            "| / \\ |\n" +
+            "_______\n";
+        }
+        static public string Thinky()
+        {
+            return
+              "_______ \n" +
+              "| _ _ |\n" +
+              "|  __ |\n" +
+              "|     |\n" +
+              "_______\n";
+        }
+    }
+
+    //
+    //
+    //
+    //
+}
---
+++ laba13_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba13_2
+{
+    delegate string d();
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            d d1;
+            while (true)
+            {
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        {
+                            d1 = new d(DrawFace.Happy);
+                            Console.WriteLine(d1());
+                            break;
+                        }
+                    case "2":
+                        {
+                            d1 = new d(DrawFace.Sad);
+                            Console.WriteLine(d1());
+                            break;
+                        }
+                    case "3":
+                        {
+                            d1 = new d(DrawFace.Thinky);
+                            Console.WriteLine(d1());
+                            break;
+                        }
+                }
+            }
+            
+        }
+    }
+}
